c90a09886350c38c970e44b205e65c9188854d55
arjunnarang-ui <arjun.narang@lhsla.org>
2026-09-12 15:28:55 -0700

    pushing assignment 1c

--- assignment 1c.py
+++ assignment 1c.py
@@ -10,9 +10,14 @@ velocityNum = float(velocity)
 KEJoules= 1/2*massKgNum*velocityNum**2
 KECalories = KEJoules/4.184
 KEErgs = KEJoules * 10**7
+# Print formatted kinetic energy report
 print ("Kinetic Energy Report for:", nameOfObjectClean)
 outputLineOne = f"Kinetic Energy Report for: {nameOfObjectClean}"
 print (outputLineOne)
 print ("-----------------------------------------------------")
-outputLineThree = f"Joules:\t{KEJoules} J"
+outputLineThree = f"Joules:\t\t{KEJoules:.2f} J"
 print (outputLineThree)
+outputLineFour  = f"Calories:\t{KECalories:.2f} cal"
+outputLineFive  = f"Ergs:\t\t{KEErgs:.2f} erg"
+print(outputLineFour)
+print(outputLineFive)
---
+++ assignment 2a.py
@@ -0,0 +1,12 @@
+# Arjun Narang 
+# Computer Programing, Period 4
+# Assignment: Homework 2a
+# 10 September 2026
+
+hobbies = ["tennis", "hanging out with friends", "reading", "videogames", "watching tv"]
+print (hobbies)
+print (len(hobbies))
+print (hobbies [1])
+print (hobbies [0])
+hello100= ["hello!"]*100
+print (hello100)
