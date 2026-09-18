848ae47b648c55bbee571a0c3a00fbd617b34307
arjunnarang-ui <arjun.narang@lhsla.org>
2026-09-18 12:13:49 -0700

    pushing  assignment 2a

--- assignment 2a.py
+++ assignment 2a.py
@@ -6,7 +6,34 @@
 hobbies = ["tennis", "hanging out with friends", "reading", "videogames", "watching tv"]
 print (hobbies)
 print (len(hobbies))
-print (hobbies [1])
+print (hobbies [2])
 print (hobbies [0])
 hello100= ["hello!"]*100
 print (hello100)
+helloHobbies=(hobbies+hello100)
+print (helloHobbies)
+favFoods = ["donuts", "chips", "in and out", "shake shack", "chipotle"]
+print (len(favFoods))
+print (favFoods[2])
+print (favFoods [-4])
+favFoods.append ("arjun")
+favFoods.remove ("in and out")
+favFoods.insert (2, 16)
+print (favFoods)
+print (list(range(1,21)))
+print (list(range(1,21,3)))
+animals = ["cat","tiger", "panther"]
+for things in animals:
+    print (things)
+    print (f"A {things} would make a great pet!")
+print ("These animals are all part of the cat kingdom.")
+people = ["Jesus", "Steve Jobs", "Tupac"]
+for peoples in people:
+    print (peoples)
+    print (f"I am inviting you to have dinner with me I would like to learn about your succes and experiences. To: {peoples}.")
+print (f"{people [2]} can't make it anymore.")
+people [2] = "Henry Ford"
+print (f"{people [2]} you have been invited to a dinner with me Jesus and Steve Jobs sorry for the late notice come to nobu at 2pm. Thank you for your time" )
+for peoples in people:
+    print (peoples)
+    print (f"I am inviting you to have dinner with me I would like to learn about your succes and experiences. To: {peoples}.") 
